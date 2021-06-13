ee146ea1c785c96edf27482d0d6344a87d854eb6
suriel_asael <kato_1350@hotmail.com>
2021-06-13 16:18:52 -0500

    project-angular_bases add: add agregar component with Output

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -19,18 +19,12 @@ export class MainPageComponent {
   ];
 
   nuevo: Personaje = {
-    nombre: '',
-    poder: 0
+    nombre: 'Maestro Rochi',
+    poder: 1000
   }
 
-  agregar() {
-    if( this.nuevo.nombre.trim().length === 0){ return; }
-
-    this.personajes.push( this.nuevo );
-    this.nuevo = {
-      nombre: '',
-      poder: 0
-    }
+  agregarNuevoPersonaje( argumento: Personaje ) {
+      this.personajes.push( argumento );
   }
   
 }
