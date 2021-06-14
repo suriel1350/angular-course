ac2be5bdfa6365af30f6887c66cc40f65e814729
suriel_asael <kato_1350@hotmail.com>
2021-06-14 17:56:38 -0500

    project-angular_bases add: add service for dbz

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,30 +1,19 @@
 import { Component } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
 
+import { DbzService } from '../services/dbz.service';
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
 
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegeta',
-      poder: 7500
-    }
-  ];
-
   nuevo: Personaje = {
     nombre: 'Maestro Rochi',
     poder: 1000
   }
 
-  agregarNuevoPersonaje( argumento: Personaje ) {
-      this.personajes.push( argumento );
-  }
+  constructor( ) {}
   
 }
